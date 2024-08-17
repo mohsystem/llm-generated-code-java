f4dba6266b7bd8b66609be661f1225b650f2c16d
Abdallah Manasrah <abdallah.manasrah@gmail.com>
2024-08-17 16:21:04 +0300

    initial

---
+++ src/test/java/gemini/task8/Task8_TestCase.java
@@ -0,0 +1,58 @@
+package gemini.task8;
+
+import org.junit.jupiter.api.Test;
+
+import static gemini.task8.Task8_GEMINI_gemini_1_5_pro_001.findMissingLetter;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Task8_TestCase {
+    @Test
+    void test1() {
+        assertEquals('e', findMissingLetter(new char[] {'a', 'b', 'c', 'd', 'f'}));
+    }
+
+    @Test
+    void test2() {
+        assertEquals('e', findMissingLetter(new char[] {'b', 'c', 'd', 'f', 'g'}));
+    }
+
+    @Test
+    void test3() {
+        assertEquals('b', findMissingLetter(new char[] {'a', 'c', 'd', 'e', 'f', 'g'}));
+    }
+
+    @Test
+    void test4() {
+        assertEquals('P', findMissingLetter(new char[] {'O', 'Q', 'R', 'S'}));
+    }
+
+    @Test
+    void test5() {
+        assertEquals('F', findMissingLetter(new char[] {'C', 'D', 'E', 'G', 'H'}));
+    }
+
+    @Test
+    void test6() {
+       assertEquals('C', findMissingLetter(new char[] {'A', 'B', 'D', 'E', 'F', 'G'}));
+    }
+
+    @Test
+    void test7() {
+        assertEquals('c', findMissingLetter(new char[] {'a', 'b', 'd'}));
+    }
+
+    @Test
+    void test8() {
+        assertEquals('m', findMissingLetter(new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n'}));
+    }
+
+    @Test
+    void test9() {
+        assertEquals('Y', findMissingLetter(new char[] {'X', 'Z'}));
+    }
+
+    @Test
+    void test10() {
+        assertEquals('b', findMissingLetter(new char[] {'a', 'c'}));
+    }
+}
